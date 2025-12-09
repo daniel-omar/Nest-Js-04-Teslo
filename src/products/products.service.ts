994c7d8ac23483f38a57234b97e90798e4bb0235
daniel-omar <omarmi15200@gmail.com>
2025-12-09 14:56:05 -0500

    actualziar producto con befofeUpdate

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -57,8 +57,21 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productsRepository.preload({
+      id: id,
+      ...updateProductDto
+    });
+    if (!product) throw new NotFoundException(`Product with id: ${id} not found`);
+    
+    try {
+      await this.productsRepository.save(product);
+
+      return product;
+
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   async remove(id: string) {
